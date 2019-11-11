be90ca13e11ddaaa9e04ff4b049a7ebcddc03ebd
unknown <emreboluk@gmail.com>
2019-11-11 19:31:16 +0300

    update

--- main.py
+++ main.py
@@ -6,33 +6,37 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
 	"""Burasi ana sayfadir"""
-	# isterseniz asagidakini aktif ederek template'deki html sayfasina yonlendirebilirsiniz.
-	# return render_template('index.html', **locals())
-	return 'Hello World!'
+	# template'deki index.html sayfasina yonlendirebilirsiniz
+	return render_template('index.html', **locals())
+	# veya isterseniz basit sekilde asagidaki gibi girebilirsiniz
+	# return 'Hello World!'
 
 
 @app.route('/sayfa1')
 def sayfa1():
 	"""Ozel sayfalar duzenleyebilirsiniz."""
-	# isterseniz asagidakini aktif ederek template'deki html sayfasina yonlendirebilirsiniz.
-	# return render_template('sayfa.html', **locals())
-	return "Sayfa1'e hos geldiniz."
+	# template'deki sayfa.html sayfasina yonlendirebilirsiniz
+	return render_template('sayfa.html', **locals())
+	# veya isterseniz basit sekilde asagidaki gibi girebilirsiniz
+	# return "Sayfa1'e hos geldiniz."
 
 
 @app.route('/sayfa2/<ozelSayfa>')
 def sayfa2(ozelSayfa):
 	"""Ozel sayfalar duzenleyebilirsiniz."""
-	# isterseniz asagidakini aktif ederek template'deki html sayfasina yonlendirebilirsiniz.
-	# return render_template('sayfa.html', **locals())
-	return "Sayfa2/%s sayfasina hos geldiniz." % ozelSayfa
+	# template'deki sayfa.html sayfasina yonlendirebilirsiniz
+	return render_template('sayfa.html', **locals())
+	# veya isterseniz basit sekilde asagidaki gibi girebilirsiniz
+	# return "Sayfa2/%s sayfasina hos geldiniz." % ozelSayfa
 
 
 @app.errorhandler(404)
 def page_not_found(e):
 	"""404 sayfalari bu fonksiyona yonlenir"""
-	# isterseniz asagidakini aktif ederek template'deki html sayfasina yonlendirebilirsiniz.
-	# return render_template('404.html', **locals()), 404
-	return 'Boyle bir sayfa bulunamadi!', 404
+	# template'deki 404.html sayfasina yonlendirebilirsiniz
+	return render_template('404.html', **locals()), 404
+	# veya isterseniz basit sekilde asagidaki gibi girebilirsiniz
+	# return 'Boyle bir sayfa bulunamadi!', 404
 
 
 # sanal sunucu veya pc'de localhost ile calistirmak icin,
